e52500856d8ea8b99f78243577dd10687923de21
Peter <pcrall04@gmail.com>
2023-12-28 16:34:06 -0800

    Made everything work part 1

--- src/main/java/com/peter/PermissionWhitelist.java
+++ src/main/java/com/peter/PermissionWhitelist.java
@@ -1,11 +1,16 @@
-package com.example;
+package com.peter;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.networking.v1.ServerPlayConnectionEvents;
+import net.fabricmc.api.networking.v1.PacketSender;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class PermissionWhitelist implements ModInitializer, ServerPlayConnectionEvents.Join {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
@@ -17,6 +22,13 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Loaded Permission Whitelist");
 	}
+
+	@Override
+	public void onPlayReady(ServerPlayNetworkHandler handler, PacketSender sender, MinecraftServer server) {
+		
+	}
+
+
 }
